972d8500d6be316b45a647bf27eec69994884e6b
julian-schulz <julian.schulz@student.hnu.de>
2024-11-27 10:25:58 +0100

    Test Commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,15 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("HI");
+
+
+
         System.out.println("HALLO VON HIEr");
     }
 
     public static int add (){
 
-        return 55;
+        return 0;
     }
 }
